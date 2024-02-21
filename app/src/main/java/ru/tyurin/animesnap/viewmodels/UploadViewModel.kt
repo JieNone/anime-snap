b04a38fb172aa6ba6fe0f76f37b3d16fa2f83042
JieNone <shepard182@mail.ru>
2024-02-22 01:48:15 +0300

    ui: timecode, episode info, text refactor

--- app/src/main/java/ru/tyurin/animesnap/viewmodels/UploadViewModel.kt
+++ app/src/main/java/ru/tyurin/animesnap/viewmodels/UploadViewModel.kt
@@ -1,11 +1,9 @@
 package ru.tyurin.animesnap.viewmodels
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import ru.tyurin.animesnap.domain.repository.AnimeTitleRepository
 import ru.tyurin.animesnap.utils.AnimeUiState
@@ -22,25 +20,20 @@ class UploadViewModel @Inject constructor(
     fun uploadImage(imageFile: File) {
         viewModelScope.launch {
             try {
-                Log.d("UPLOAD_VIEW_MODEL", "Start uploading image: $imageFile")
                 val response = repository.searchByImage(imageFile)
                 if (response.isSuccessful) {
                     val animeTitle = response.body()
                     uiState.value = AnimeUiState.Success(animeTitle!!)
                 }
-                Log.d("UPLOAD_VIEW_MODEL", "Upload completed successfully")
             } catch (e: IOException) {
-                Log.e("UPLOAD_VIEW_MODEL", "IOException occurred: $e")
+
                 uiState.value = AnimeUiState.Error
             } catch (e: NullPointerException) {
-                Log.e("UPLOAD_VIEW_MODEL", "NullPointerException occurred: $e")
                 uiState.value = AnimeUiState.Error
             } catch (e: retrofit2.HttpException) {
-                Log.e("UPLOAD_VIEW_MODEL", "HttpException occurred: $e")
                 if (e.code() == 400) {
                     uiState.value = AnimeUiState.Error
                 } else {
-                    Log.e("UPLOAD_VIEW_MODEL", "Unknown HttpException occurred: $e")
                     uiState.value = AnimeUiState.Error
                 }
             }
